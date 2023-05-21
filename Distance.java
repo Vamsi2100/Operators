08ae4a898dd208fb9d1295aaf446752f0bbda577
Vamsi2100 <Vamsifighter2108@gmail.com>
2023-05-21 20:55:17 +0530

    Added: Distance.java file

---
+++ Distance.java
@@ -0,0 +1,8 @@
+public class Distance {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;
+        double d = Math.sqrt(x*x + y*y);
+        System.out.println(d);
+    }
+}
