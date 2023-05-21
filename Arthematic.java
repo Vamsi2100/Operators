da78224904e882008e35330f443d1f7165fcaafd
Vamsi2100 <Vamsifighter2108@gmail.com>
2023-05-21 20:53:52 +0530

    Added: Arthematic.java file for roots

---
+++ Arthematic.java
@@ -0,0 +1,12 @@
+public class Arthematic {
+    public static void main(String[] args) {
+        int a = 21;
+        int b = 8;
+        int c = 02;
+
+        System.out.println(a+b*c);
+        System.out.println(c+a/b);
+        System.out.println(a%b+c);
+        System.out.println(a*b+c);
+    }
+}
